4cbf3ebb156dcc15f5b1155dc47ffe7a1a6c67dd
michael kaburu <michael.kaburu@student.moringaschool.com>
2022-08-28 19:53:47 +0300

    done

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,21 @@
+# # Your code goes here!
+# listen = Anagram.new("listen")
+# listen.match(%w["enlists google inlets banana"])
 # Your code goes here!
+class Anagram
+    attr_reader :word
+    def initialize (word)
+        @word = word
+    end
+    def match(arr)
+        matched = []
+        arr.map do |word|
+            if word.chars.sort == @word.chars.sort
+              matched.push(word)
+            
+            end
+        end
+        matched
+    end
+end
+
